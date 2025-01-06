af049292584df318cefeaa0e02d8e8ba02000d32
Timothy Dake <spaco67@gmail.com>
2025-01-06 07:31:34 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'okiapp',
+  webDir: 'dist'
+};
+
+export default config;
